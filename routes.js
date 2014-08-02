6c01e533ef466ee8954298ae3132aee85e64efc6
Ciara Burkett <ciaraburkett@gmail.com>
2014-08-02 10:47:06 -0400

    Add relative paths for static pages and absolute path to Medium blog in header nav

--- routes.js
+++ routes.js
@@ -1,4 +1,8 @@
 Router.map(function() {
    this.route('home', {path: '/'});
    this.route('dashboard', {path: '/dashboard'});
+   this.route('about', {path: '/about'});
+   this.route('faq', {path: '/faq'});
+   this.route('contact', {path: '/contact'});
+   this.route('support', {path: '/support'});
 });
