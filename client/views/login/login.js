246138a78dc2bd82b369e6d1c8a0b1a14ab30d47
Ciara Burkett <ciaraburkett@gmail.com>
2014-08-04 19:08:47 -0400

    Change class attribute for email and password input elements

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,6 +1,19 @@
 Template.login.events({
    'submit .login': function(event, template) {
       event.preventDefault();
+
+      var email = template.find('.log-email').value;
+      var password = template.find('.log-password').value;
+
+      Meteor.loginWithPassword(email, password, function(error) {
+         if (error) {
+            console.log('ERROR ERROR ERROR!');
+         }
+         else {
+            console.log('User is logged in.');
+         }
+      });
+
       Router.go('home');
    }
 })
