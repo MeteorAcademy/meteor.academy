c2cd31fca76b3f6a5357af5a3f4c040c5ebe7606
Ciara Burkett <ciaraburkett@gmail.com>
2014-08-23 21:09:23 -0400

    Service configuration, again

--- server/startup.js
+++ server/startup.js
@@ -5,7 +5,7 @@ Meteor.startup(function() {
 
    Accounts.loginServiceConfiguration.insert({
       service: 'meteor-developer',
-      credentialToken: 'HqBrJkT6D8u2osBES',
-      credentialSecret: 'sc9ba5HNx5M6jxDhTFLomdN4PRotnMXLnA'
+      clientId: 'HqBrJkT6D8u2osBES',
+      secret: 'sc9ba5HNx5M6jxDhTFLomdN4PRotnMXLnA'
    });
 });
