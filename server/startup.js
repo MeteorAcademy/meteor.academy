5fd9033e5b66cdb6a764b2695ce3ac657c43a85b
Ciara Burkett <ciaraburkett@gmail.com>
2014-08-23 21:05:00 -0400

    Remove service meteor developer service configuration

--- server/startup.js
+++ server/startup.js
@@ -1,7 +1,11 @@
 Meteor.startup(function() {
+   Accounts.loginServiceConfiguration.remove({
+      service: 'meteor-developer'
+   });
+
    Accounts.loginServiceConfiguration.insert({
       service: 'meteor-developer',
-      clientId: 'HqBrJkT6D8u2osBES',
-      secret: 'sc9ba5HNx5M6jxDhTFLomdN4PRotnMXLnA'
+      credentialToken: 'HqBrJkT6D8u2osBES',
+      credentialSecret: 'sc9ba5HNx5M6jxDhTFLomdN4PRotnMXLnA'
    });
 });
